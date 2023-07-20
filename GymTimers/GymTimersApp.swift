56c0295e51e77ff0047c14a20be162e1e3517e89
elexion <jeffreyklinkhamer541@hotmail.com>
2023-07-20 23:02:46 +0200

    Initial Commit

---
+++ GymTimers/GymTimersApp.swift
@@ -0,0 +1,20 @@
+//
+//  GymTimersApp.swift
+//  GymTimers
+//
+//  Created by Jeffrey Klinkhamer on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GymTimersApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
